8d88058b33bc8b8a1f570ae68ecd1dfe2d1d8366
Lucas Antognoni <antognoni.lucas@gmail.com>
2018-07-04 12:42:04 -0300

    Emergency fragment and other fixes

--- app/src/main/java/br/usp/icmc/healthpal/healthpal/EmergencyListFragment.java
+++ app/src/main/java/br/usp/icmc/healthpal/healthpal/EmergencyListFragment.java
@@ -1,23 +1,86 @@
 package br.usp.icmc.healthpal.healthpal;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import br.usp.icmc.healthpal.healthpal.components.EmergencyCard;
+
 public class EmergencyListFragment extends Fragment {
 
-    @Nullable
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_emergency_list, container, false);
+    }
+
+
+    @Nullable
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+
+        EmergencyCard hospital = getView().findViewById(R.id.hospital);
+        EmergencyCard firefighters = getView().findViewById(R.id.firefighters);
+        EmergencyCard police = getView().findViewById(R.id.police);
+        EmergencyCard contact = getView().findViewById(R.id.emergencyContact);
+
+        hospital.setOnClickListener((e) -> {
+            Intent callIntent = new Intent(Intent.ACTION_CALL);
+            callIntent.setData(Uri.parse("tel:0377778888"));
+
+            if (ActivityCompat.checkSelfPermission(this.getContext(),
+                    Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+
+            this.startActivity(callIntent);
+        });
+
+        firefighters.setOnClickListener((e) -> {
+            Intent callIntent = new Intent(Intent.ACTION_CALL);
+            callIntent.setData(Uri.parse("tel:0377778888"));
+
+            if (ActivityCompat.checkSelfPermission(this.getContext(),
+                    Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+
+            this.startActivity(callIntent);
+        });
+
+        police.setOnClickListener((e) -> {
+            Intent callIntent = new Intent(Intent.ACTION_CALL);
+            callIntent.setData(Uri.parse("tel:0377778888"));
+
+            if (ActivityCompat.checkSelfPermission(this.getContext(),
+                    Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+
+            this.startActivity(callIntent);
+        });
+
+        contact.setOnClickListener((e) -> {
+            Intent callIntent = new Intent(Intent.ACTION_CALL);
+            callIntent.setData(Uri.parse("tel:0377778888"));
+
+            if (ActivityCompat.checkSelfPermission(this.getContext(),
+                    Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+
+            this.startActivity(callIntent);
+        });
+
     }
 }
