5cf1869d12404d91dafcc94a71d6fa83bfe21dfc
santiagogustavo <santiago.gustavo.mail@gmail.com>
2018-07-04 00:04:23 +0000

    (+) Add state handling to save current fragment

--- app/src/main/java/br/usp/icmc/healthpal/healthpal/MainActivity.java
+++ app/src/main/java/br/usp/icmc/healthpal/healthpal/MainActivity.java
@@ -7,14 +7,22 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Fragment mFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (savedInstanceState != null) {
+            mFragment = getSupportFragmentManager().getFragment(savedInstanceState, "currentFragment");
+        } else {
+            mFragment = new DashboardFragment();
+        }
+
         BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new DashboardFragment()).commit();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, mFragment).commit();
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -22,15 +30,28 @@ public class MainActivity extends AppCompatActivity {
         Fragment selected = null;
 
         switch (item.getItemId()) {
-            case R.id.navigation_home:
-                selected = new DashboardFragment();
-                break;
             case R.id.navigation_medicine:
                 selected = new MedicineListFragment();
                 break;
+            default:
+                selected = new DashboardFragment();
+                break;
         }
 
+        mFragment = selected;
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selected).commit();
         return true;
     };
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        mFragment = getSupportFragmentManager().getFragment(savedInstanceState, "currentFragment");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        getSupportFragmentManager().putFragment(savedInstanceState, "currentFragment", mFragment);
+    }
 }
